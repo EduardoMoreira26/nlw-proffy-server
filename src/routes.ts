40d29830704c2c01bfec5be22f8397e599f034b1
Eduardo <du_du.mba@hotmail.com>
2021-01-07 22:28:42 +0000

    create routes

---
+++ src/routes.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+
+const routes = express.Router();
+
+
+routes.post('/classes', (request, response) => {
+  const {
+    name,
+    avatar,
+    whatsapp,
+    bio,
+    subject,
+    cost,
+    schedule
+  } = request.body;
+
+
+  return response.send();
+});
+
+export default routes;
